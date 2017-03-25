b856329cb9a51a1071a7e6d03c21ade3f3fcd6dc
1966366130 <1966366130@qq.com>
2017-03-25 23:59:38 +0800

    learn __call__

---
+++ samples/specialclass/special_call.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 _8_
+
+class Student(object):
+	def __init__(self,name):
+		self.name=name
+
+	def __call__(self):
+		print('My name is %s.' % self.name)
+
+s=Student('huaihaizhi')
+s()
