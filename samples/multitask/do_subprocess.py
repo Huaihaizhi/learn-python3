a8664e8e317ebec42ef83273dc2daa4cb7e09527
1966366130 <1966366130@qq.com>
2017-04-04 15:33:53 +0800

    learn subprocess/queue

---
+++ samples/multitask/do_subprocess.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import subprocess 
+print('$ nslookup www.python.org')
+r=subprocess.call(['nslookup','www.python.org'])
+print('Exit code:',r)
+
+print('$ nslookup')
+p=subprocess.Popen(['nslookup'],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE,)
+output,err=p.communicate(b'set q=mx\npython.org\nexit\n')
+print(output.decode('latin1').encode('utf-8'))
+print('Exit code:',p.returncode)
