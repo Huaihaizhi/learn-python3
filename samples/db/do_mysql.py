c770bb296d86e5caf6fc8b88b96ab4cd35a21c7c
1966366130 <1966366130@qq.com>
2017-04-23 17:58:27 +0800

    learn mysql

--- samples/db/do_mysql.py
+++ samples/db/do_mysql.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 _*_
 
 import mysql.connector
-conn=mysql.connector.connect(user='root',password='shengfang1115',database='test')
+conn=mysql.connector.connect(user='root',password='',database='test')
 cursor=conn.cursor()
 cursor.execute('create table user3 (id varchar(20) primary key,name varchar(20))')
 cursor.execute('insert into user (id,name) values (%s,%s)',('3','huaihaizhi'))
